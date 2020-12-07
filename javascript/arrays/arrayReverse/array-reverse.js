0c350896347b8c4a9d3643c75f31cc1c7e5590bc
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-12-07 15:47:25 -0700

    code challenge 1

---
+++ javascript/arrays/arrayReverse/array-reverse.js
@@ -0,0 +1,11 @@
+let array = [1,2,3];
+
+const reverseArray = array => {
+  let invertedArray = [];
+  for (let i=0;i<array.length; i++){
+    invertedArray.unshift(array[i]);
+  }
+  return invertedArray;
+};
+
+reverseArray(array);
