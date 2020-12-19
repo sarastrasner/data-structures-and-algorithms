92f0e0828519cf80c94c8a874ce46400e912af2d
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-12-19 10:24:05 -0700

    linked list updates

--- javascript/linkedList/linkedList/linked-list.js
+++ javascript/linkedList/linkedList/linked-list.js
@@ -1,28 +1,27 @@
-'use strict';
+"use strict";
 
-const Node = require('./node');
+const Node = require("./node");
 
-class LinkedList{
-  constructor(value){
+class LinkedList {
+  constructor(value) {
     this.value = value;
     this.head = null;
   }
 
-  isEmpty(){
+  isEmpty() {
     return this.head === null ? true : false;
   }
 
-  insert(value){
+  insert(value) {
     //insert a node at the beginning of a given LL
 
-    if (this.isEmpty() === true){
+    if (this.isEmpty() === true) {
       // the list is empty
       // make a new Node with the value given
       const node = new Node(value);
       //   // assign this.head to that new node
       this.head = node;
-    }
-    else if (this.isEmpty() === false) {
+    } else if (this.isEmpty() === false) {
       // the list is NOT empty
       // make a new Node with the value given
       const node = new Node(value);
@@ -38,46 +37,45 @@ class LinkedList{
     // traverse the entire list until I find the one whose next is null
 
     let currentNode = this.head;
-    while(currentNode.next !== null){
+    while (currentNode.next !== null) {
       currentNode = currentNode.next;
     }
     // when I find that one, make the next my new node
     currentNode.next = node;
     // make my new node's next null - already done
-
   }
-  includes(val){
+  includes(val) {
     // Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
     let currentNode = this.head;
     // start at the head
-    while(currentNode){
-    // iterate through the whole list until you hit a return statement
-      if (currentNode.value === val){
+    while (currentNode) {
+      // iterate through the whole list until you hit a return statement
+      if (currentNode.value === val) {
         return true;
-      // if the current node contains the value, return true
-      } else if (!currentNode.value === val){
+        // if the current node contains the value, return true
+      } else if (!currentNode.value === val) {
         currentNode = currentNode.next;
         // if the current node DOES NOT contain the value, keep iterating
-      } if (currentNode.next === null){
+      }
+      if (currentNode.next === null) {
         return false;
         // if you reach the end of the linked list, return false
       }
     }
-
   }
-  toString(){
+  toString() {
     // Define a method called toString which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
     // "{ a } -> { b } -> { c } -> NULL"
-    let runningValue = '';
+    let runningValue = "";
     let currentNode = this.head;
-    while(currentNode){
+    while (currentNode) {
       // iterate through the whole list
       runningValue += `{ ${currentNode.value} } -> `;
       // add this vallue to the runningValue
       currentNode = currentNode.next;
       // keep iterating
     }
-    runningValue += 'NULL';
+    runningValue += "NULL";
     return runningValue;
   }
 
@@ -110,16 +108,17 @@ class LinkedList{
   }
 
   kthFromEnd(k) {
-    if (k < 0){
-      return 'Exception';
+    if (k < 0) {
+      return "Exception";
     } else {
       let currentNode = this.head;
       let index = 0;
       while (currentNode) {
         index = index + 1;
-        currentNode= currentNode.next;
-      } if (index < k) {
-        return 'Exception';
+        currentNode = currentNode.next;
+      }
+      if (index < k) {
+        return "Exception";
       }
       let countdown = index - k - 1;
       currentNode = this.head;
@@ -128,7 +127,6 @@ class LinkedList{
       }
       return currentNode.value;
     }
-
   }
 }
 
@@ -142,7 +140,7 @@ class LinkedList{
 //list2: head -> [5] -> [9] -> [4] -> X
 //output: head -> [1] -> [5] -> [3] -> [9] -> [4] -> X
 
-function zipLists (list1,list2){
+function zipLists(list1, list2) {
   // This is list1: { apple } -> { banana } -> NULL
   // This is list2: { peach } -> { pear } -> NULL
   // WANT:  { apple } -> { peach } -> { banana } -> { pear } -> NULL'
@@ -161,7 +159,6 @@ function zipLists (list1,list2){
 
   //WANT:  { apple } -> { peach } -> { banana } -> { pear } -> NULL'
 
-
   // let currentNode1 = list1.head;
 
   // let currentNode2 = list2.head;
@@ -178,8 +175,6 @@ function zipLists (list1,list2){
   // return list1;
 }
 
-
-module.exports = {zipLists: zipLists, Node: Node, LinkedList: LinkedList};
-
+module.exports = { zipLists: zipLists, Node: Node, LinkedList: LinkedList };
 
 //module.exports = LinkedList;
